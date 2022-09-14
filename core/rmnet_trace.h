3f271daa4536ea4e11efd66afe072e7682b84a46
Kaustubh Pandey <quic_kapandey@quicinc.com>
2022-09-14 19:23:20 +0530

    rmnet: Fix compilation issue for newer LE devices with kernel upgrade
    Rmnet core compilation was failing due to wrong dir being picked in
    trace headers. This change fixes this by using correct path based on
    kernel version.
    
    Change-Id: I8ae1bf9a3320349298f616ad2a8de0d7e16b0198
    Signed-off-by: Kaustubh Pandey <quic_kapandey@quicinc.com>

--- core/rmnet_trace.h
+++ core/rmnet_trace.h
@@ -1,16 +1,17 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /* Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 #include <linux/version.h>
 #undef TRACE_SYSTEM
 #define TRACE_SYSTEM rmnet
 #undef TRACE_INCLUDE_PATH
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5,5,0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5,16,0)
 #ifdef CONFIG_ARCH_SDXNIGHTJAR
 #define TRACE_INCLUDE_PATH ../../../../../../../datarmnet/core
 #else
 #define TRACE_INCLUDE_PATH ../../../../../../../src/datarmnet/core
-#endif /* endif LINUX_VERSION_CODE < KERNEL_VERSION(5,5,0) */
+#endif /* endif LINUX_VERSION_CODE < KERNEL_VERSION(5,16,0) */
 #else
 #define TRACE_INCLUDE_PATH ../../../../vendor/qcom/opensource/datarmnet/core
 #endif

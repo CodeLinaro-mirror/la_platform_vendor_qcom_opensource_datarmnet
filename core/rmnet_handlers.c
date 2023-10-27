c462cb0c53c2095ef9a600a2b380bce869408f25
Abhinav Kannan <quic_kannana@quicinc.com>
2023-10-26 23:17:25 -0700

    Revert "core: Skip partial checksum operations for ip route ip call"
    This reverts commit 4c0b091fa762b8d59ca6ec4a5b92b49cc322271d.
    
    Change-Id: I7b791f94e5f1f4899b76e5f4952c48385a172ca2
    Signed-off-by: Abhinav Kannan <quic_kannana@quicinc.com>

--- core/rmnet_handlers.c
+++ core/rmnet_handlers.c
@@ -228,9 +228,6 @@ static void rmnet_ip_route_rcv(struct sk_buff *skb, struct rmnet_port *port)
 		if (!ep)
 			goto drop_skb;
 
-		if (ep->call_type == RMNET_IP_ROUTE_CALL_TYPE_IP)
-			break;
-
 		proto = ip6h->nexthdr;
 		ip_len = ipv6_skip_exthdr(skb, sizeof(*ip6h), &proto,
 					  &frag_off);

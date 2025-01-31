CodeLinaro-mirror/la_platform_vendor_qcom_opensource_datarmnet

commit 8f8fdd7a23d706d946e3d0fbb5bc97972551961f

datarmnet: Add support for sdxkova target
Add changes for compilation.

CRs-Fixed: 3981160
Change-Id: I285b8152201b376b9518e09002f8b99ea490e947
Signed-off-by: Kaustubh Pandey <quic_kapandey@quicinc.com>

core/rmnet_vnd.c
@@ -26,7 +26,11 @@
 #include <linux/ethtool.h>
 #include <linux/ipa.h>
 #include <net/pkt_sched.h>
+#include <linux/version.h>
 #include <net/ipv6.h>
+#if (KERNEL_VERSION(6, 5, 0) <= LINUX_VERSION_CODE)
+#include <net/gso.h>
+#endif
 #include <net/xfrm.h>
 #include "rmnet_config.h"
 #include "rmnet_handlers.h"
@@ -115,8 +119,8 @@ static netdev_tx_t rmnet_vnd_start_xmit(struct sk_buff *skb,
 
 		if ((priv->real_dev->features & NETIF_F_HW_ESP) &&
 		    (priv->real_dev->hw_enc_features & NETIF_F_HW_ESP) &&
-		    (skb->ipa_skb_cb.magic == IPA_IPSEC_SKB_MAGIC)) {
-			ipsec = skb->ipa_skb_cb.sa_dir;
+		    (IPA_IPSEC_SKB_CB(skb)->magic == IPA_IPSEC_SKB_MAGIC)) {
+			ipsec = IPA_IPSEC_SKB_CB(skb)->sa_dir;
 			priv->stats.ul_ipsec++;
 		}
 
@@ -255,12 +259,12 @@ static void rmnet_get_stats64(struct net_device *dev,
 		pcpu_ptr = per_cpu_ptr(priv->pcpu_stats, cpu);
 
 		do {
-			start = u64_stats_fetch_begin_irq(&pcpu_ptr->syncp);
+			start = u64_stats_fetch_begin(&pcpu_ptr->syncp);
 			total_stats.rx_pkts += pcpu_ptr->stats.rx_pkts;
 			total_stats.rx_bytes += pcpu_ptr->stats.rx_bytes;
 			total_stats.tx_pkts += pcpu_ptr->stats.tx_pkts;
 			total_stats.tx_bytes += pcpu_ptr->stats.tx_bytes;
-		} while (u64_stats_fetch_retry_irq(&pcpu_ptr->syncp, start));
+		} while (u64_stats_fetch_retry(&pcpu_ptr->syncp, start));
 
 		total_stats.tx_drops += pcpu_ptr->stats.tx_drops;
 	}
@@ -719,6 +723,8 @@ static const struct ethtool_ops rmnet_ethtool_ops = {
 	.nway_reset = rmnet_stats_reset,
 };
 
+#ifdef CONFIG_XFRM
+
 static bool rmnet_xfrm_is_valid_state(struct xfrm_state *x)
 {
 	struct rmnet_priv *priv = NULL;
@@ -847,6 +853,8 @@ static const struct xfrmdev_ops rmnet_xfrmdev_ops = {
 	.xdo_dev_policy_free = rmnet_xfrm_policy_free,
 };
 
+#endif //#ifdef CONFIG_XFRM
+
 /* Called by kernel whenever a new rmnet<n> device is created. Sets MTU,
  * flags, ARP type, needed headroom, etc...
  */
@@ -855,7 +863,7 @@ void rmnet_vnd_setup(struct net_device *rmnet_dev)
 	rmnet_dev->netdev_ops = &rmnet_vnd_ops;
 	rmnet_dev->mtu = RMNET_DFLT_PACKET_SIZE;
 	rmnet_dev->needed_headroom = RMNET_NEEDED_HEADROOM;
-	random_ether_addr(rmnet_dev->perm_addr);
+	eth_hw_addr_random(rmnet_dev);
 	rmnet_dev->tx_queue_len = RMNET_TX_QUEUE_LEN;
 
 	/* Raw IP mode */
@@ -890,6 +898,7 @@ int rmnet_vnd_newlink(u8 id, struct net_device *rmnet_dev,
 	rmnet_dev->hw_features |= NETIF_F_GRO_HW;
 	rmnet_dev->hw_features |= NETIF_F_GSO_UDP_L4;
 	rmnet_dev->hw_features |= NETIF_F_ALL_TSO;
+#ifdef CONFIG_XFRM
 
 	if ((real_dev->features & NETIF_F_HW_ESP) &&
 	    (real_dev->hw_enc_features & NETIF_F_HW_ESP)) {
@@ -898,6 +907,7 @@ int rmnet_vnd_newlink(u8 id, struct net_device *rmnet_dev,
 		rmnet_dev->features |= NETIF_F_HW_ESP;
 		rmnet_dev->hw_enc_features |= NETIF_F_HW_ESP;
 	}
+#endif //#ifdef CONFIG_XFRM
 
 	priv->real_dev = real_dev;
 
@@ -962,7 +972,7 @@ void rmnet_vnd_reset_mac_addr(struct net_device *dev)
 	if (dev->netdev_ops != &rmnet_vnd_ops)
 		return;
 
-	random_ether_addr(dev->perm_addr);
+	eth_hw_addr_random(dev);
 }
 
 int netif_is_rmnet(const struct net_device *dev)

core/Kbuild
@@ -2,7 +2,7 @@ ifneq (, $(filter y, $(CONFIG_ARCH_LAHAINA) $(CONFIG_ARCH_WAIPIO) $(CONFIG_ARCH_
 ccflags-y	+= -DRMNET_LA_PLATFORM
 endif
 
-ifeq (y, $(filter y, $(CONFIG_ARCH_SDXNIGHTJAR) $(CONFIG_ARCH_SDXPINN) $(CONFIG_ARCH_SDXBAAGHA)))
+ifeq (y, $(filter y, $(CONFIG_ARCH_SDXNIGHTJAR) $(CONFIG_ARCH_SDXPINN) $(CONFIG_ARCH_SDXBAAGHA) $(CONFIG_ARCH_SDXKOVA)))
 ccflags-y	+= -DRMNET_TRACE_INCLUDE_LE
 endif
 

core/rmnet_config.c
@@ -901,10 +901,10 @@ void rmnet_get_packets(void *port, u64 *rx, u64 *tx)
 		for_each_possible_cpu(cpu) {
 			ps = per_cpu_ptr(priv->pcpu_stats, cpu);
 			do {
-				start = u64_stats_fetch_begin_irq(&ps->syncp);
+				start = u64_stats_fetch_begin(&ps->syncp);
 				*tx += ps->stats.tx_pkts;
 				*rx += ps->stats.rx_pkts;
-			} while (u64_stats_fetch_retry_irq(&ps->syncp, start));
+			} while (u64_stats_fetch_retry(&ps->syncp, start));
 		}
 	}
 	rcu_read_unlock();

core/rmnet_config.h
@@ -15,6 +15,7 @@
  */
 
 #include <linux/skbuff.h>
+#include <linux/version.h>
 #include <net/gro_cells.h>
 #include "rmnet_map.h"
 

core/rmnet_descriptor.c
@@ -287,9 +287,13 @@ int rmnet_frag_descriptor_add_frag(struct rmnet_frag_descriptor *frag_desc,
 
 	INIT_LIST_HEAD(&frag->list);
 	get_page(p);
+#if (KERNEL_VERSION(6, 5, 0) > LINUX_VERSION_CODE)
 	__skb_frag_set_page(&frag->frag, p);
 	skb_frag_size_set(&frag->frag, len);
 	skb_frag_off_set(&frag->frag, page_offset);
+#else
+	skb_frag_fill_page_desc(&frag->frag, p, page_offset, len);
+#endif
 	list_add_tail(&frag->list, &frag_desc->frags);
 	frag_desc->len += len;
 	return 0;

core/rmnet_eth_main.c
@@ -1,5 +1,5 @@
 /* Copyright (c) 2013-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -13,7 +13,7 @@
  * RMNET_ETH main handler
  *
  */
-
+#include <linux/etherdevice.h>
 #include <linux/module.h>
 #include <linux/skbuff.h>
 #include <linux/netdevice.h>
@@ -287,12 +287,12 @@ static void rmnet_veth_get_stats64(struct net_device *dev,
 		pcpu_ptr = per_cpu_ptr(priv->pcpu_stats, cpu);
 
 		do {
-			start = u64_stats_fetch_begin_irq(&pcpu_ptr->syncp);
+			start = u64_stats_fetch_begin(&pcpu_ptr->syncp);
 			total_stats.rx_frames += pcpu_ptr->stats.rx_frames;
 			total_stats.rx_bytes += pcpu_ptr->stats.rx_bytes;
 			total_stats.tx_frames += pcpu_ptr->stats.tx_frames;
 			total_stats.tx_bytes += pcpu_ptr->stats.tx_bytes;
-		} while (u64_stats_fetch_retry_irq(&pcpu_ptr->syncp, start));
+		} while (u64_stats_fetch_retry(&pcpu_ptr->syncp, start));
 
 		total_stats.tx_drops += pcpu_ptr->stats.tx_drops;
 		total_stats.rx_drops += pcpu_ptr->stats.rx_drops;
@@ -434,7 +434,7 @@ rx_handler_result_t rmnet_eth_rx_handler(struct sk_buff **pskb,
 	dev = skb->dev;
 	port = rmnet_eth_get_port_rtnl(dev);
 	if (unlikely(!port)) {
-		atomic_long_inc(&skb->dev->rx_nohandler);
+		dev_core_stats_rx_nohandler_inc(skb->dev);
 		rmnet_eth_drop_rx_skb(skb, RMNET_ETH_RX_INV_PORT);
 		goto done;
 	}
@@ -523,7 +523,7 @@ static int rmnet_eth_register_real_device(struct net_device *real_dev)
 void rmnet_eth_setup(struct net_device *rmnet_eth_dev)
 {
 	ether_setup(rmnet_eth_dev);
-	random_ether_addr(rmnet_eth_dev->dev_addr);
+	eth_hw_addr_random(rmnet_eth_dev);
 	rmnet_eth_dev->netdev_ops = &rmnet_veth_ops;
 	rmnet_eth_dev->needed_headroom = RMNET_ETH_NEEDED_HEADROOM;
 }

core/rmnet_handlers.c
@@ -787,7 +787,7 @@ rx_handler_result_t rmnet_rx_handler(struct sk_buff **pskb)
 	dev = skb->dev;
 	port = rmnet_get_port(dev);
 	if (unlikely(!port)) {
-		atomic_long_inc(&skb->dev->rx_nohandler);
+		dev_core_stats_rx_nohandler_inc(skb->dev);
 		kfree_skb(skb);
 		goto done;
 	}

core/rmnet_ll_qmap.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -60,7 +61,7 @@ struct qmap_ll_switch {
 	u8			reserved;
 	u8			request_type;
 	u8			num_bearers;
-	struct qmap_ll_bearer	bearer[0];
+	struct qmap_ll_bearer	bearer[];
 } __aligned(1);
 
 struct qmap_ll_switch_resp {
@@ -68,7 +69,7 @@ struct qmap_ll_switch_resp {
 	u8			cmd_ver;
 	u8			reserved[2];
 	u8			num_bearers;
-	struct qmap_ll_bearer	bearer[0];
+	struct qmap_ll_bearer	bearer[];
 } __aligned(1);
 
 struct qmap_ll_switch_status {
@@ -76,7 +77,7 @@ struct qmap_ll_switch_status {
 	u8			cmd_ver;
 	u8			reserved[2];
 	u8			num_bearers;
-	struct qmap_ll_bearer	bearer[0];
+	struct qmap_ll_bearer	bearer[];
 } __aligned(1);
 
 /*

commit cb3d6d92c1b75378403a6dded879a5b7840b824e

core: Add back the new ipsec offload detection
Unfortunately,
commit 8f8fdd7a23d7 ("datarmnet: Add support for sdxkova target")
inadvertently reverted the changes made by
commit 36d93a422c53 ("core: Change ipsec offload packet detection
scheme).

CRs-Fixed: 4032456
Change-Id: I54011bff70f25b582dcea0c8b1c6d7ddd2245b46
Signed-off-by: Subash Abhinov Kasiviswanathan <subash.a.kasiviswanathan@oss.qualcomm.com>

core/rmnet_vnd.c
@@ -1,5 +1,5 @@
 /* Copyright (c) 2013-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -117,12 +117,14 @@ static netdev_tx_t rmnet_vnd_start_xmit(struct sk_buff *skb,
 		if (RMNET_APS_LLC(skb->priority))
 			low_latency = true;
 
+#if IS_ENABLED(CONFIG_IPA3)
 		if ((priv->real_dev->features & NETIF_F_HW_ESP) &&
 		    (priv->real_dev->hw_enc_features & NETIF_F_HW_ESP) &&
-		    (IPA_IPSEC_SKB_CB(skb)->magic == IPA_IPSEC_SKB_MAGIC)) {
-			ipsec = IPA_IPSEC_SKB_CB(skb)->sa_dir;
+		    (skb->ipa_skb_cb.magic == IPA_IPSEC_SKB_MAGIC)) {
+			ipsec = skb->ipa_skb_cb.sa_dir;
 			priv->stats.ul_ipsec++;
 		}
+#endif /* IS_ENABLED(CONFIG_IPA3) */
 
 		if ((low_latency || RMNET_APS_LLB(skb->priority)) &&
 		    skb_is_gso(skb) && !ipsec) {